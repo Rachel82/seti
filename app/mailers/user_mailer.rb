7ef270f6e2cc8cc9d9de5a9d3c5a2bb7ee910c52
Rachel82 <akilimali82@hotmail.fr>
2018-02-08 19:30:32 +0100

    6.15 Fixed code error in user Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
 	default from: "akilimali82@hotmail.fr" #change to admin
 
 	def contact_form(email,name, message)
-		@message(from: email,
+		@message = message
+			mail(from: email,
 			to: 'rachel_akilimali@hotmail.com',
 			subject: 'A new contact form message from #{name}')
 	end
