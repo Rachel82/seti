68122d32a23d84d845186d4d5b0c2cf85c5c1cfc
Rachel82 <akilimali82@hotmail.fr>
2018-02-09 14:40:13 +0100

    6.15 Run db:migrate, change Image tag code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :category
 	  has_attached_file :image, styles: { large: "900x900>", medium: "300x300>", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
-	  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 	def self.search(search_term)
 		like_string = Rails.env.production? ? "ilike" : "LIKE"
